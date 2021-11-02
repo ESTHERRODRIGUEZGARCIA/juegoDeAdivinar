e641dad87e538eba5f81aa50f6aabfe471db3989
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-02 12:16:49 +0100

    hay que poner dos bucles while (mientars que)

--- juegoDeAdivinar.py
+++ juegoDeAdivinar.py
@@ -1,3 +1,15 @@
 import random
-numero = random.randint(0, 99)
+numero = random.randint(0, 100)
 intento = int(input("Introduce un número: "))
+intento = True
+while intento == True:
+    if intento < numero:
+        print("Demasiado pequeño")
+    elif intento == numero:
+        print("¡Has ganado!")
+    else:
+        print("Demasiado grande ")
+    intento = False
+    print("Introduce otro numero: ")
+
+
