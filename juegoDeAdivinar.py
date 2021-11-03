668fa23a2722467d5413dcf9c8f41e989dbea7d2
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-03 12:52:29 +0100

    segundo while que poner?

--- juegoDeAdivinar.py
+++ juegoDeAdivinar.py
@@ -2,14 +2,16 @@ import random
 numero = random.randint(0, 100)
 intento = int(input("Introduce un número: "))
 intento = True
+intento = 1
 while intento == True:
-    if intento < numero:
+    while 
+        if intento < numero:
         print("Demasiado pequeño")
     elif intento == numero:
         print("¡Has ganado!")
     else:
         print("Demasiado grande ")
-    intento = False
-    print("Introduce otro numero: ")
+    intento = intento + 1
+    print("Has realizado ", intento)
 
 
