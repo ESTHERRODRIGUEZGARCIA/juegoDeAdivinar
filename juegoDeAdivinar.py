5e6b6a912ccca51283c53937862d2023414909c4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-03 13:01:51 +0100

    subir

--- juegoDeAdivinar.py
+++ juegoDeAdivinar.py
@@ -1,17 +1,18 @@
 import random
 numero = random.randint(0, 100)
-intento = int(input("Introduce un número: "))
-intento = True
+intento = int(input("Adivina el número: "))
 intento = 1
+intento = True
 while intento == True:
-    while 
-        if intento < numero:
+    intento = intento + 1
+    if intento < numero:
         print("Demasiado pequeño")
     elif intento == numero:
         print("¡Has ganado!")
     else:
         print("Demasiado grande ")
-    intento = intento + 1
-    print("Has realizado ", intento)
+
+print("Has realizado ", intento, "intentos")
+
 
 
