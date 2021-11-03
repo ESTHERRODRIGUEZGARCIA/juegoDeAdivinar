a0b394a3a280bb98c417a613be1e3705e15a14ea
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-03 15:00:18 +0100

    segundo intento

--- juego2probando.py
+++ juego2probando.py
@@ -6,15 +6,18 @@ intentosRealizados = 0
 
 intento = int(input("Adivina el número: "))
 
-while intento != numero :
-    intentosRealizados = intentosRealizados + 1   
+while True:
+    intentosRealizados = intentosRealizados + 1
+    print("Has ganado")
+    if intento==numero:
+        break 
     if intento < numero:
         print("Demasiado pequeño ")
         print("Introduce otro número: ")
     if intento > numero:
         print("Demasiado grande ")  
         print("Introduce otro número: ")
-    if intento == numero:
-        break 
+
 #mensaje fuera
 print("Enhorabuena, has realizado ", intentosRealizados, "intentos")
+
