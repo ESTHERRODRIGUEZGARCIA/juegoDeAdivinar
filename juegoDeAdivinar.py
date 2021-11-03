3f5642118c073b4bd0804678e4da12a69e5bf22b
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-03 13:45:17 +0100

    como poner el break ¡!

--- juegoDeAdivinar.py
+++ juegoDeAdivinar.py
@@ -1,20 +1,23 @@
 import random
 numero = random.randint(0, 100)
-intento = 0
+intentosRealizados = 0
+
 intento = int(input("Adivina el número: "))
-while intento != numero:
-    intento = intento + 1
+
+while intento != numero :
+    intentosRealizados = intentosRealizados + 1   
     if intento < numero:
-        print("Demasiado pequeño")
-    if intento > numero:
-        print("Demasiado grande ")
-    if intento == numero:
-        break
+        print("Demasiado pequeño ")
+        print("Introduce otro número: ")
+    elif intento > numero:
+        print("Demasiado grande ")  
+        print("Introduce otro número: ")
+    else:
+        print("¡Has ganado en tu primer intento!")
+        break      
+    print("Enhorabuena, has realizado ", intentosRealizados, "intentos")
 
 if intento == numero:
-    print("¡Has ganado!")
-
-print("Has realizado ", intento, "intentos")
-
+    print("¡Has ganado en tu primer intento!")
 
 
