edee1292a834e73766789d93da903b86320f090e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-11-12 19:57:37 +0100

    mas cambios, que se me olvidan los commiiiitssss

--- terminando_juego.py
+++ terminando_juego.py
@@ -1,51 +1,60 @@
 
 print("""
     Vamos a jugar al juego de adivinar un número. 
+
     1) Nivel de dificultad simple (entre 0 y 100)
+
     2) Nivel de dificultad intermedio (entre 0 y 1.000)
+
     3) Nivel de dificultad avanzado (entre 0 y 1.000.000)
+
     4) Nivel de dificultad experto (entre 0 y 1.000.000.000.000)
+
 """)
 
-nivel = int(input("Seleccione un nivel de dificultad, 1 2 3 o 4: "))
+nivel = int(input("Seleccione un nivel de dificultad, 1 2 3 o 4: \n "))
+if nivel > 4:
+    print("\nError. Hay 4 posibles niveles. Seleccione de nuevo el nivel: ")
+elif nivel < 1 :
+    print("\nError. Hay 4 posibles niveles. Seleccione de nuevo el nivel: ")
+
+
 
 if nivel == 1:
-    print("¡Empezamos!")
+    print("¡Empezamos! Has elegido el nivel simple")
 
     import random
     numero = random.randint(0, 100)
     intentosRealizados = 0
-    intento = int(input("Adivina el número: "))
+    maxintento = 25
+    
+    intento = int(input("Adivine el número: \n"))
 
-if intento < 0 or intento > 100:
-    intentosRealizados = intentosRealizados + 1
-    print("Error; el número debe estar entre el 0 y el 99 ")
-if intento < numero:
-    intentosRealizados = intentosRealizados + 1
-    print("Demasiado pequeño ")
-if intento > numero:
-    intentosRealizados = intentosRealizados + 1
-    print("Demasiado grande ")
-while intento != numero :
-    print("intenta adivinar el número: ")
-    intento = int(input()) 
     if intento < 0 or intento > 100:
         intentosRealizados = intentosRealizados + 1
-        print("Error; el número debe estar entre el 0 y el 99 ")
+        print("Error, el número debe estar entre el 0 y el 99 ")
     if intento < numero:
         intentosRealizados = intentosRealizados + 1
-        print("Demasiado pequeño ")
+        print("Demasiado pequeño. ")
     if intento > numero:
         intentosRealizados = intentosRealizados + 1
-        print("Demasiado grande ")
+        print("Demasiado grande. ")
+    while intento != numero :
+        print("Intenta adivinar el número: ")
+        intento = int(input()) 
+        if intento < 0 or intento > 100:
+            intentosRealizados = intentosRealizados + 1
+            print("Error, el número debe estar entre el 0 y el 99 ")
+        if intento < numero:
+            intentosRealizados = intentosRealizados + 1
+            print("Demasiado pequeño. ")
+        if intento > numero:
+            intentosRealizados = intentosRealizados + 1
+            print("Demasiado grande. ")
 
-        if intento == numero:
-            break
+            if intento == numero:
+                break
     
 
 #mensaje fuera
-print("¡Enhorabuena! Has acertado el número. Has realizado ", intentosRealizados, "intentos.")
-
-
-
-
+    -print("¡Enhorabuena! Has acertado el número. Has realizado ", intentosRealizados, "intentos.")
